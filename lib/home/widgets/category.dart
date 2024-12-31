1a89df9d43f5c4333f64f6b9939c72806ecac168
Majekodunmi David <majekodunmidavid05@gmail.com>
2024-12-31 21:00:48 +0100

    category updated

--- lib/home/widgets/category.dart
+++ lib/home/widgets/category.dart
@@ -1,20 +1,22 @@
-// import 'package:flutter/material.dart';
-//
-// class Categories extends StatelessWidget {
-//   const Categories({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return SizedBox(
-//       height: 130,
-//       child: ListView.separated(
-//         scrollDirection: Axis.horizontal,
-//         itemCount: categories,
-//         itemBuilder: (context, index) {
-//
-//         },
-//         separatorBuilder: (context, index) => const SizedBox(width: 20,),
-//       ),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+
+import '../../models/category.dart';
+
+class Categories extends StatelessWidget {
+  const Categories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 130,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+
+        },
+        separatorBuilder: (context, index) => const SizedBox(width: 20,),
+      ),
+    );
+  }
+}
--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,2 +1,34 @@
 import 'package:flutter/material.dart';
 
+class Category {
+  final String title;
+  final String image;
+
+  Category ({
+    required this.title,
+    required this.image,
+});
+}
+
+final List<Category> categories = [
+  Category(
+      title: "Shoes",
+      image: "assets/images/shoes.png",
+  ),
+  Category(
+    title: "Beauty",
+    image: "assets/images/beauty.png",
+  ),
+  Category(
+    title: "Women's Fashion",
+    image: "assets/images/image1.png",
+  ),
+  Category(
+    title: "Jewelry",
+    image: "assets/images/jewelry.png",
+  ),
+  Category(
+    title: "Women's Fashion ",
+    image: "assets/images/men.png",
+  ),
+];
